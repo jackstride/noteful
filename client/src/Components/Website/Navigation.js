de48c71af69f350f150b1016714106afdbedda07
Jack Stride <jackstride@jacks-mbp.home>
2019-12-13 18:04:39 +0000

    redesign

--- client/src/Components/Website/Navigation.js
+++ client/src/Components/Website/Navigation.js
@@ -1,22 +1,40 @@
 import React, { Component } from "react";
 import { Route, Link } from "react-router-dom";
 
-import Logo from '../../images/noteful_logo_blue.svg'
+import Logo from "../../images/noteful_bw.svg";
 
 export default class Navigation extends Component {
   render() {
     return (
       <div className="web_nav">
-          <img alt='NoteFul Logo' src={Logo}></img>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/dashboard">Features</Link>
-              <Link to="/dashboard">About</Link>
-              <Link to="/dashboard">Support</Link>
-            </li>
-          </ul>
-        </nav>
+        <div className="inner_container center_center">
+          <img alt="NoteFul Logo" src={Logo}></img>
+          <nav class="website_nav">
+            <ul>
+              <li>
+                <Link to="/dashboard">Features</Link>
+              </li>
+              <li>
+                <Link to="/dashboard">About</Link>
+              </li>
+              <li>
+                <Link to="/dashboard">Support</Link>
+              </li>
+            </ul>
+          </nav>
+          <nav className="website_nav_login">
+            <ul>
+              <li>
+                <Link to="/dashboard">Sign Up</Link>
+              </li>
+              <span className="circle_border">
+              <li>
+                <Link to="/dashboard">Login</Link>
+              </li>
+              </span>
+            </ul>
+          </nav>
+        </div>
       </div>
     );
   }
--- client/src/Home.js
+++ client/src/Home.js
@@ -1,29 +1,48 @@
-import React, { Component } from 'react'
-import {Redirect} from 'react-router-dom'
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 
-import Navigation from './Components/Website/Navigation'
+import Navigation from "./Components/Website/Navigation";
 
-import Pablo from './images/pablo-downloading.png'
+import Pablo from "./images/app_preview.jpg";
 
 export default class home extends Component {
-
-    handleSubmit = (e) => {
-        e.preventDefault();
-        this.props.history.push('/register')
-    }
-    render() {
-        return (
-            <div className="home_app_container">
-                <Navigation />
-                <div className="welcome_page">
-                    <h1> Get <span>Organised</span><br></br> with NoteFul...</h1>
-                    <h2>Sign up today. Free of charge.</h2>
-                    <input type="button" onClick={this.handleSubmit} value="Register"></input>                    
-                    <img src={Pablo}></img>
-
-                </div>
-                
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.history.push("/register");
+  };
+  render() {
+    return (
+      <div className="home_app_container">
+        <Navigation />
+        <div className="welcome_page">
+          <div className="BG_SHAPES">
+            <div className="circle shape_style"></div>
+            <div className="rectangle shape_style"></div>
+          </div>
+          <div className="header_tagline">
+            <h1>
+              {" "}
+              Get <span>Organised</span>
+              <br></br> with noteFul..
+            </h1>
+            <h3>Sign up today. Free of charge.</h3>
+            <input
+              type="button"
+              onClick={this.handleSubmit}
+              value="Register"
+            ></input>
+          </div>
+          <div className="app_preview_image">
+            <img src={Pablo} href="noteful Preview"></img>
+          </div>
+          <div className="feature_container shape_style">
+            <div className="inner_container">
+              <h2>Features</h2>
+              <p>Noteful is built with simple user interface keeping the user in mind. Easy access to notes</p>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
