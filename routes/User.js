59f73226cfde14641f553f64c565c8d4f7a4c350
Jack Stride <jackstride@jacks-mbp.home>
2020-02-07 19:56:36 +0000

    Cleaing

--- routes/User.js
+++ routes/User.js
@@ -8,93 +8,81 @@ const { userValidationRules, validate } = require("../middleware/validation");
 
 const User = require("../models/User");
 
-router.post("/register", userValidationRules(), validate, (req, res, next) => {
-  console.log("user Routes");
-  let { firstName, lastName, email, password } = req.body.formValues;
+router.post(
+  "/register",
+  userValidationRules(),
+  validate,
+  async (req, res, next) => {
+    let { firstName, lastName, email, password } = req.body.formValues;
 
-  let saltRounds = 15;
+    let saltRounds = 15;
 
-  User.find({ email })
-    .exec()
-    .then(user => {
-      if (user.length) {
-        next(createError(409, "User Exists with this email"));
-      } else {
-        bcrypt.genSalt(saltRounds, (err, salt) => {
-          if (err) {
-            next(newcreateError(500, "Problem with server"));
-          }
-          bcrypt.hash(password, salt).then(hash => {
-            password = hash;
-            const user = new User({
+    try {
+      let user = await User.find({ email }).exec();
+
+      user.length
+        ? next(createError(409, "User Exists with this email"))
+        : (salt = await bcrypt.genSaltSync(saltRounds));
+
+      salt
+      ? hash = await bcrypt.hash(password, salt)
+      : next(createError(500, " Salt Server Error"))
+      hash
+      ? user = new User({
               _id: new mongoose.Types.ObjectId(),
               firstName: firstName,
               lastName: lastName,
               email: email,
-              password: password
-            });
-            user.save().then(result => {
-              res.status(201).json({
-                message: "Register Successful"
-              });
-            });
-          });
+              password: hash
+            })
+      : next(createError(500, " Database register error"))
+
+      let result = await user.save()
+
+      result
+      ? res.status(201).json({ message: "Register Successful"})
+      : next(newcreateError(500, "Problem with server"))
+    } catch (err) {}
+  }
+);
+
+router.post("/login", async (req, res, next) => {
+
+  let { email, password } = req.body.formValues;
+
+  console.log(email,password)
+
+  let user = await User.find({email}).exec();
+  user.length < 1 
+  ? next(createError(401, " Authorisation Failed"))
+  :  match = await bcrypt.compare(password,user[0].password)
+
+  if(match) {
+    const payload = {
+      email: user[0].email,
+      _id: user[0].id
+    };
+
+    jtw.sign(payload,process.env.JWT_KEY,
+      {
+        expiresIn: "1h"
+      }, (err, token) => {
+        res.cookie("access_token", token, {
+          maxAge: 9000000,
+          httpOnly: true
         });
-      }
-    });
+        res.status(200).json({ user: payload });
+      })
+  }
+  else {
+     next(createError(401, " Password doesn't match")) 
+  } 
 });
 
-router.post("/login", (req, res) => {
-  let { email, password } = req.body;
 
-  User.find({
-    email
-  })
-    .exec()
-    .then(user => {
-      if (user.length < 1) {
-        return res.status(401).json({
-          message: "Auth Failed"
-        });
-      }
 
-      bcrypt.compare(password, user[0].password, (err, result) => {
-        if (err) {
-          return res.status(401).json({
-            message: "Auth Failed"
-          });
-        }
 
-        if (result) {
-          const payload = {
-            email: user[0].email,
-            _id: user[0].id
-          };
 
-          jtw.sign(
-            payload,
-            process.env.JWT_KEY,
-            {
-              expiresIn: "1h"
-            },
-            (err, token) => {
-              res.cookie("access_token", token, {
-                maxAge: 9000000,
-                httpOnly: true
-              });
-              res.status(200).json({ user: payload });
-            }
-          );
-        }
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        err
-      });
-    });
-});
 
 router.get("/logout", (req, res) => {
   res.clearCookie("access_token").sendStatus(200);
