654be97d0883d57001ff095d4ba12d6ec1eb1aa3
Jack Stride <jackstride@jacks-mbp.home>
2020-02-20 20:09:34 +0000

    test

--- client/src/Components/Dashboard/Home.js
+++ client/src/Components/Dashboard/Home.js
@@ -3,21 +3,21 @@ import { connect } from "react-redux";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import SideNav from "../SideNav";
 import { loadUser } from "../../actions/authActions";
-import Editor from '../TextEditor/Editor'
+import Editor from "../TextEditor/Editor";
 
 import Settings from "./Settings";
 
 class Home extends Component {
   render() {
     return (
-        <div className="app_container">
-      <BrowserRouter>
-      <SideNav />
-        <Switch>
-          <Route path="/dashboard/settings" component={Settings} />
-          <Route path="/dashboard/notes/" component={Editor} />
-        </Switch>
-      </BrowserRouter>
+      <div className="app_container">
+        <BrowserRouter>
+          <SideNav />
+          <Switch>
+            <Route path="/dashboard/settings" component={Settings} />
+            <Route path="/dashboard/" component={Editor} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
--- client/src/Components/TextEditor/Editor.js
+++ client/src/Components/TextEditor/Editor.js
@@ -1,24 +1,126 @@
-import React, { Component, Fragment } from "react";
-const Quill = require('quill')
+import React, { useEffect, useMemo, useState, useCallback } from "react";
+import { Editable, withReact, useSlate, Slate } from "slate-react";
+import { Editor, Transforms, createEditor, Text } from "slate";
 
-export default class Editor extends Component {
-  componentDidMount() {
-    var editor = new Quill('#editor', {
-        modules: { toolbar: '#toolbar' },
-        theme: 'snow'
-      });
-  }
-  render() {
-    return (
-      <div className="editor_container">
-        <div id="toolbar">
-          <button class="ql-bold">Bold</button>
-          <button class="ql-italic">Italic</button>
-        </div>
-        <div id="editor">
-          <p>Hello World!</p>
-        </div>
-      </div>
+const TextEditor = props => {
+  const editor = useMemo(() => withReact(createEditor()), []);
+  // Add the initial value when setting up our state.
+  const [value, setValue] = useState([
+    {
+      type: "paragraph",
+      children: [{ text: "A line of text in a paragraph." }]
+    }
+  ]);
+
+  const renderElement = useCallback(props => <Element {...props} />, []);
+  const renderLeaf = useCallback(props => <Leaf {...props} />, []);
+
+  return (
+    <Slate editor={editor} value={value} onChange={value => setValue(value)}>
+      <Editable
+        renderElement={renderElement}
+        renderLeaf={renderLeaf}
+        onKeyDown={event => {
+          if (!event.ctrlKey) {
+            return;
+          }
+
+          // Replace the `onKeyDown` logic with our new commands.
+          switch (event.key) {
+            case "a": {
+              event.preventDefault();
+              CustomEditor.toggleCodeBlock(editor);
+              break;
+            }
+
+            case "b": {
+              event.preventDefault();
+              CustomEditor.toggleBoldMark(editor);
+              break;
+            }
+          }
+        }}
+      />
+    </Slate>
+  );
+};
+
+const CustomEditor = {
+  isBoldMarkActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: n => n.bold === true,
+      universal: true
+    });
+
+    return !!match;
+  },
+
+  isCodeBlockActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: n => n.type === "code"
+    });
+
+    return !!match;
+  },
+
+  toggleBoldMark(editor) {
+    const isActive = CustomEditor.isBoldMarkActive(editor);
+    Transforms.setNodes(
+      editor,
+      { bold: isActive ? null : true },
+      { match: n => Text.isText(n), split: true }
+    );
+  },
+
+  toggleCodeBlock(editor) {
+    const isActive = CustomEditor.isCodeBlockActive(editor);
+    Transforms.setNodes(
+      editor,
+      { type: isActive ? null : "code" },
+      { match: n => Editor.isBlock(editor, n) }
     );
   }
-}
+};
+
+const Element = ({ attributes, children, element }) => {
+  switch (element.type) {
+    case "block-quote":
+      return <blockquote {...attributes}>{children}</blockquote>;
+    case "bulleted-list":
+      return <ul {...attributes}>{children}</ul>;
+    case "heading-one":
+      return <h1 {...attributes}>{children}</h1>;
+    case "heading-two":
+      return <h2 {...attributes}>{children}</h2>;
+    case "list-item":
+      return <li {...attributes}>{children}</li>;
+    case "numbered-list":
+      return <ol {...attributes}>{children}</ol>;
+    default:
+      return <p {...attributes}>{children}</p>;
+  }
+};
+
+const Leaf = ({ attributes, children, leaf }) => {
+  if (leaf.bold) {
+    children = <strong>{children}</strong>;
+  }
+
+  if (leaf.code) {
+    children = <code>{children}</code>;
+  }
+
+  if (leaf.italic) {
+    children = <em>{children}</em>;
+  }
+
+  if (leaf.underline) {
+    children = <u>{children}</u>;
+  }
+
+  let test = <span {...attributes}>{children}</span>;
+  console.log(test);
+  return test;
+};
+
+export default TextEditor;
