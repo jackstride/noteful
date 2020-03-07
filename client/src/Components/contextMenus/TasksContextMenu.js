f59c9e394f823a323dfc2ae5e59e26bd9c5a1c8c
Jackstride <jackstride@outlook.com>
2020-03-07 16:33:48 +0000

    Fixing small bugs

--- client/src/Components/contextMenus/TasksContextMenu.js
+++ client/src/Components/contextMenus/TasksContextMenu.js
@@ -1,60 +1,74 @@
-import React, { Component } from 'react'
-import {connect} from 'react-redux'
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import {toggleOpenTask,removeTask, toggleTask} from '../../actions/taskActions'
-import {hideMenu,showMenu} from '../../actions/contextMenuActions'
+import {
+  toggleOpenTask,
+  removeTask,
+  toggleTask
+} from "../../actions/taskActions";
+import { hideMenu, showMenu } from "../../actions/contextMenuActions";
 
 class TasksContextMenu extends Component {
+  handleRename = () => {
+    this.props.showMenu(
+      this.props.x,
+      this.props.y,
+      "EditContextMenu",
+      { name: this.props.name, id: this.props.id },
+      "tasks"
+    );
+  };
 
+  handleDelete = (e, id) => {
+    e.preventDefault();
+    this.props.removeTask(id);
+    this.props.hideMenu();
+  };
 
+  handleMarkTask = (e, id) => {
+    e.preventDefault();
+    this.props.toggleTask(id);
+    this.props.hideMenu();
+  };
 
-    handleRename = () => {
-        this.props.showMenu(this.props.x,this.props.y,"EditContextMenu",{name: this.props.name,id: this.props.id}, "tasks")
-    }
+  handleToggleAdd = () => {
+    this.props.toggleOpenTask();
+    this.props.hideMenu();
+  };
 
-    handleDelete = (e,id) => {
-        e.preventDefault();
-        console.log('hello')
-        this.props.removeTask(id);
-    }
-
-    handleMarkTask = (e,id) => {
-        e.preventDefault();
-        this.props.toggleTask(id);
-    }
-
-    render() {
-        return (
-            <ul>
-                <li onClick={this.props.toggleOpenTask}> Add Tasks</li>
-                <li onClick={(e) => this.handleRename(e,this.props.id)}>Rename Task</li>
-                <li onClick={(e) => this.handleDelete(e,this.props.id)}> Delete Task</li>
-                <li onClick={(e) => this.handleMarkTask(e,this.props.id)}>Mark Task</li>
-            </ul>            
-        )
-    }
+  render() {
+    return (
+      <ul>
+        <li
+          onClick={() => {
+            this.handleToggleAdd();
+          }}
+        >
+          Add Tasks
+        </li>
+        <li onClick={e => this.handleRename(e, this.props.id)}>Rename Task</li>
+        <li onClick={e => this.handleDelete(e, this.props.id)}> Delete Task</li>
+        <li onClick={e => this.handleMarkTask(e, this.props.id)}>Mark Task</li>
+      </ul>
+    );
+  }
 }
-
 
 const mapStateToProps = state => {
-    return {
-        id: state.contextMenu.menuArgs.id,
-        x: state.contextMenu.location.x,
-        y: state.contextMenu.location.y,
-    }
-}
-
+  return {
+    id: state.contextMenu.menuArgs.id,
+    x: state.contextMenu.location.x,
+    y: state.contextMenu.location.y
+  };
+};
 
 const mapDispatchToProps = dispatch => ({
-    toggleOpenTask: () => dispatch(toggleOpenTask()),
-    hideMenu: () => dispatch(hideMenu()),
-    removeTask: id => dispatch(removeTask(id)),
-    toggleTask: id => dispatch(toggleTask(id)),
-    showMenu: (x,y,getType,args,name) => dispatch(showMenu(x,y,getType,args,name)),
-  });
+  toggleOpenTask: () => dispatch(toggleOpenTask()),
+  hideMenu: () => dispatch(hideMenu()),
+  removeTask: id => dispatch(removeTask(id)),
+  toggleTask: id => dispatch(toggleTask(id)),
+  showMenu: (x, y, getType, args, name) =>
+    dispatch(showMenu(x, y, getType, args, name))
+});
 
-
-
-export default connect (mapStateToProps,mapDispatchToProps)(TasksContextMenu)
-
-
+export default connect(mapStateToProps, mapDispatchToProps)(TasksContextMenu);
