cabf485c17ff503d6aa3fc0c39fe98319144d5a6
Jackstride <jackstride@outlook.com>
2020-03-10 21:32:35 +0000

    test

--- client/src/Components/Website/Navigation.js
+++ client/src/Components/Website/Navigation.js
@@ -1,59 +1,76 @@
-import React, { Component, Fragment, useState } from "react";
-import { Link } from "react-router-dom";
+import React, { Fragment, useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
 import { connect } from "react-redux";
 
 import Logo from "../../images/noteful_bw.svg";
 
-class Navigation extends Component {
-  render() {
-    return (
-      <div className="website_navigation">
-        <div className="inner_container center_center">
-          <Link to="/">
-            <img alt="NoteFul Logo" src={Logo}></img>
-          </Link>
-          <nav className="website_nav">
-            <ul>
-              <li>
-                <Link to="/features">Features</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/support">Support</Link>
-              </li>
-            </ul>
-          </nav>
-          <nav className="website_nav_login">
-            <ul>
-              {!this.props.auth ? (
-                <Fragment>
-                  <li>
-                    <Link to="/register">Sign Up</Link>
-                  </li>
-                  <span className="circle_border">
-                    <Link to="/login">Login</Link>
-                  </span>
-                </Fragment>
-              ) : (
-                <Fragment>
-                  <li>
-                    <Link to="/dashboard">Dashboard</Link>
-                  </li>
-                  <li>
-                    <Link to="/logout">Logout</Link>
-                  </li>
-                </Fragment>
-              )}
-            </ul>
-          </nav>
-        </div>
-        <Mobilenav auth={this.props.auth} />
+const Navigation = ({ auth }) => {
+  const [light, isLight] = useState(false);
+  const history = useHistory();
+
+  // Tooo lonmgggg
+  useEffect(() => {
+    console.log(history.location);
+    if (
+      history.location.pathname.includes("reg") ||
+      history.location.pathname.includes("log")
+    ) {
+      isLight(true);
+    } else {
+      isLight(false);
+    }
+  }, [history.location.pathname]);
+
+  return (
+    <div
+      style={light ? { backgroundColor: "white" } : { backgroundColor: "blue" }}
+      className="website_navigation"
+    >
+      <div className="inner_container center_center">
+        <Link to="/">
+          <img alt="NoteFul Logo" src={Logo}></img>
+        </Link>
+        <nav className="website_nav">
+          <ul>
+            <li>
+              <Link to="/features">Features</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/support">Support</Link>
+            </li>
+          </ul>
+        </nav>
+        <nav className="website_nav_login">
+          <ul>
+            {!auth ? (
+              <Fragment>
+                <li>
+                  <Link to="/register">Sign Up</Link>
+                </li>
+                <span className="circle_border">
+                  <Link to="/login">Login</Link>
+                </span>
+              </Fragment>
+            ) : (
+              <Fragment>
+                <li>
+                  <Link to="/dashboard">Dashboard</Link>
+                </li>
+                <li>
+                  <Link to="/logout">Logout</Link>
+                </li>
+              </Fragment>
+            )}
+          </ul>
+        </nav>
       </div>
-    );
-  }
-}
+      <Mobilenav auth={auth} />
+    </div>
+  );
+};
 
 const mapStateToProps = state => {
   return {
@@ -61,7 +78,7 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps, null)(Navigation);
+export default connect(mapStateToProps)(Navigation);
 
 let Mobilenav = ({ auth }) => {
   let [open, isOpen] = useState(false);
