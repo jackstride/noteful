6d973a432256a3f2d59a16b6fffd9f93543c5691
Jackstride <jackstride@outlook.com>
2020-03-12 10:16:36 +0000

    test

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,10 @@ const functions = require("firebase-functions");
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "hello" });
+});
+
 app.get("/users", (req, res) => {
   res.status(200).json({ message: "hello" });
 });
