1e595db17072dc853e5cbc1b9277bd5aca9273e8
Jackstride <jackstride@outlook.com>
2020-03-28 13:45:25 +0000

    test

--- client/src/Components/TextEditor/TextEditorInsert.js
+++ client/src/Components/TextEditor/TextEditorInsert.js
@@ -3,29 +3,28 @@ import { Portal } from "react-portal";
 import { Range, Editor, Transforms } from "slate";
 import { useSlate, ReactEditor } from "slate-react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Icon, InlineIcon } from "@iconify/react";
+import headingH1 from "@iconify/icons-gridicons/heading-h1";
+import headingH2 from "@iconify/icons-gridicons/heading-h2";
+import headingH3 from "@iconify/icons-gridicons/heading-h3";
 
 const TextEditorIsnert = ({}) => {
   let [show, setShow] = useState(false);
-  let [options, setOptions] = useState([
+  let [icons, setOptions] = useState([
     {
       type: "heading-one",
       text: "Heading One",
-      icon: "times"
+      icon: headingH1
     },
     {
       type: "heading-two",
       text: "Heading Two",
-      icon: "times"
+      icon: headingH2
     },
     {
       type: "heading-three",
       text: "Heading Three",
-      icon: "times"
-    },
-    {
-      type: "list-item",
-      text: "List",
-      icon: "times"
+      icon: headingH3
     }
   ]);
   const editor = useSlate();
@@ -43,11 +42,12 @@ const TextEditorIsnert = ({}) => {
   return (
     <div className="editor_insert">
       <div onClick={() => setShow(!show)} className="icon">
+        <h5>Insert</h5>
         <FontAwesomeIcon icon="chevron-down" color="black" />
       </div>
       {show ? (
         <div className="content">
-          {options.map((item, index) => {
+          {icons.map((item, index) => {
             return (
               <div
                 onClick={() => {
@@ -56,7 +56,7 @@ const TextEditorIsnert = ({}) => {
                 key={index}
                 className="content_background"
               >
-                <FontAwesomeIcon icon={item.icon} size="lg" color="white" />
+                <Icon width="1.5em" icon={item.icon} />
               </div>
             );
           })}
