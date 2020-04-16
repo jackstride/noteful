4a64aa8f9649443b79d5cfc50b2e531cc92f1862
Jackstride <jackstride@outlook.com>
2020-04-16 14:43:55 +0100

    test

--- client/src/index.js
+++ client/src/index.js
@@ -41,29 +41,4 @@ render(
 //   });
 // }
 
-async function subscribe() {
-  console.log("rasn");
-  let sw = await navigator.serviceWorker.ready;
-  let push = await sw.seriveWorker.pushManager.subscribe({
-    userVisibleOnly: true,
-    applicationServerKey: urlBase64ToUint8Array(
-      process.env.REACT_APP_VAPID_PUBLIC
-    ),
-  });
-  console.log(JSON.stringify(push));
-}
-
-function urlBase64ToUint8Array(base64String) {
-  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
-  const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
-
-  const rawData = window.atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
-
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-  return outputArray;
-}
-
 serviceWorker.register();
